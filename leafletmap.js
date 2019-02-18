3e6b153f3b2c8571675599136337d4a538b071a8
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2019-02-18 13:48:01 +0100

    begin java server

--- leafletmap.js
+++ leafletmap.js
@@ -421,3 +421,23 @@
            
              document.getElementById("chartCollection").style.visibility = "hidden";
          }
+         
+         
+         
+         
+    var connection = new XMLHttpRequest ();
+
+    var connectionstring = "Data Source=elabsqldb2.db.11460868.hostedresource.com;Initial Catalog=elabsqldb2;User ID=elabsqldb2;password=elab17TRACK#14";
+    connection.open("GET",connectionstring);
+    var rs = new XMLHttpRequest;
+
+    rs.open("SELECT * FROM demo", connection);
+    rs.MoveFirst
+    while (!rs.eof) {
+        document.write(rs.fields(1));
+        rs.movenext;
+    }
+
+    rs.close;
+    connection.close;
+
