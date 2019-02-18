708f959ce98719025c091d30b5bfd50e97a3e912
r0629748 <thomas.desaranno@student.ucll.be>
2019-02-18 13:18:49 +0100

    slider

--- leafletmap.js
+++ leafletmap.js
@@ -315,6 +315,17 @@
          });
          map.addControl(controlSearch);
 
+        sliderControl = L.control.sliderControl({
+            position: "bottom",
+            layer: markersTemp,
+            range: true
+        });
+
+        //Make sure to add the slider to the map ;-)
+        map.addControl(sliderControl);
+        //And initialize the slider
+        sliderControl.startSlider();
+
 
          var cityname = "stad";
 
