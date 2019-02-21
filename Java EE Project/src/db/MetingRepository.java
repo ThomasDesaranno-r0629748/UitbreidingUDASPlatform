f023ac9e590b82c06c93a03435bc1da86fc0b21a
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2019-02-21 16:58:10 +0100

    Werkende server

--- Java EE Project/src/db/MetingRepository.java
+++ Java EE Project/src/db/MetingRepository.java
@@ -86,77 +86,24 @@ public class MetingRepository {
        finalData.add(data.get(0));
         for (Meting m:data){
             ArrayList<Meting> gelijkeMetingen = new ArrayList<>();
-            ArrayList<String> tijden = new ArrayList<>();
-            tijden.add("test");
-            double so2 = 0;
-            double no2 = 0;
-            double o3 = 0;
-            double pm10 = 0;
-            int amount = 0;
             for (Meting me:data){
                 if (m.getDate().equals(me.getDate()) && m.getTime().equals(me.getTime())){
                     gelijkeMetingen.add(me);
-                    /*amount++;
-                    so2 = so2 + me.getSO2();
-                    no2 = no2 + me.getNO2();
-                    o3 = o3 + me.getO3();
-                    pm10 = pm10 + me.getPM10();
-                    gelijkeM.add(me);*/
-                } /*else {
-                    amount++;
-                    //System.out.print(" date and time not equal nr "+amount+"\n" );
-                    try{
-                        if (finalData.size() == 0){
-                            System.out.print("SIZE EMPTY\n");
-                            finalData.add(me);
-                        } else {
-                            //System.out.print(" NOT NULL \n");
-                            boolean add = false;
-                            for (Meting met:finalData){
-                                if (!met.getDate().equals(me.getDate()) && !met.getTime().equals(me.getTime())){
-                                    add = true;
-                                    ///System.out.print("ADDED\n");
-                                }
-                            }
-                            if (add){
-                                finalData.add(me);
-                            }
-                        }
-                    } catch (Exception e){
-                        System.out.print("FOUT:\n");
-                        System.out.print(e.getMessage()+"\n");
-                        System.out.print(e.getClass().toString()+"\n");
-                    }
-
-                }*/
+                }
             }
             if (gelijkeMetingen.size() == 0){
                 System.out.print("SIZE NULL:\n");
                 finalData.add(m);
             } else {
                 boolean add = true;
-                Meting meting = new Meting();
                 for (Meting met: finalData){
-
                     if(met.getMetingId()==gelijkeMetingen.get(0).getMetingId()) add = false;
-                    /*for(Meting gM: gelijkeMetingen){
-                        //System.out.print(gM.getDeviceId()+"\n");
-                        if (met.getMetingId() != gM.getMetingId() && !met.getDate().equals(gM.getDate()) && !met.getTime().equals(gM.getTime())){
-                            //System.out.print("GOED\n");
-                            meting = gM;
-                            add = true;
-                        }
-                    }*/
                 }
-                //if (add) finalData.add(gelijkeMetingen.get(0));
                 if (add){
                     finalData.add(gelijkeMetingen.get(0));
                 }
             }
         }
-
-        System.out.print("TESTTESTTESTTEST "+finalData.size());
-
        return finalData;
    }
 }
--- charts.js
+++ charts.js
@@ -1,5 +1,5 @@
 let tempChart = document.getElementById("tempChart").getContext('2d');
-console.log(tempChart+"teststetetstst");
+console.log(tempChart + "teststetetstst");
 let pressureChart = document.getElementById("pressureChart").getContext('2d');
 let O3Chart = document.getElementById("O3Chart").getContext('2d');
 let PM1Chart = document.getElementById("PM1Chart").getContext('2d');
@@ -10,41 +10,58 @@ var chartDataPressure = [];
 var chartDataO3 = [];
 var chartDataPM1 = [];
 
-d3.json("http://localhost:8080/Controller?action=returnLast24hData", function (data) {
-    data.forEach(function (d) {
-        chartLabels.push(d.time);
-        chartDataTemp.push(d.so2);
-        chartDataPressure.push(d.no2);
-        chartDataO3.push(d.o3);
-        chartDataPM1.push(d.pm10);
-    })
-    console.log(chartLabels)
-    console.log(chartDataTemp)
-})
+getData();
 setTimeout(createChart, 1000, chartLabels, chartDataTemp, tempChart, 'SO2', 'rgba(255, 255, 0, 0.58)', true, '#989800');
 setTimeout(createChart, 1000, chartLabels, chartDataPressure, pressureChart, 'NO2', 'rgba(255, 0, 0, 0.58)', true, '#980000');
 setTimeout(createChart, 1000, chartLabels, chartDataPressure, O3Chart, 'O3', 'rgba(0, 255, 10, 0.58)', true, '#009806');
 setTimeout(createChart, 1000, chartLabels, chartDataPressure, PM1Chart, 'PM1', 'rgba(0, 245, 255, 0.58)', true, '#009298');
 
+setInterval(function(){
+    setTimeout(createChart, 1000, chartLabels, chartDataTemp, tempChart, 'SO2', 'rgba(255, 255, 0, 0.58)', true, '#989800');
+setTimeout(createChart, 1000, chartLabels, chartDataPressure, pressureChart, 'NO2', 'rgba(255, 0, 0, 0.58)', true, '#980000');
+setTimeout(createChart, 1000, chartLabels, chartDataPressure, O3Chart, 'O3', 'rgba(0, 255, 10, 0.58)', true, '#009806');
+setTimeout(createChart, 1000, chartLabels, chartDataPressure, PM1Chart, 'PM1', 'rgba(0, 245, 255, 0.58)', true, '#009298');
+    getData()
+}, 5*1000);
+
+function getData() {
+    
+    chartLabels = [];
+    chartDataTemp = [];
+    chartDataPressure = [];
+    chartDataO3 = [];
+    chartDataPM1 = []
+    d3.json("http://localhost:8080/Controller?action=returnLast24hData", function (data) {
+        console.log(data)
+        data.forEach(function (d) {
+            chartLabels.push(d.time);
+            chartDataTemp.push(d.so2);
+            chartDataPressure.push(d.no2);
+            chartDataO3.push(d.o3);
+            chartDataPM1.push(d.pm10);
+        })
+    })
+}
+
 function createChart(chartLabels, chartData, chart, label, backgroundcolor, beginAtZero, borderColor) {
     let LineChart = new Chart(chart, {
-        type: 'line'
-        , data: {
-            labels: chartLabels
-            , datasets: [{
-                label: label
-                , data: chartData
-                , backgroundColor: backgroundcolor
-                , pointRadius: 0
-                , borderColor: borderColor
+        type: 'line',
+        data: {
+            labels: chartLabels,
+            datasets: [{
+                label: label,
+                data: chartData,
+                backgroundColor: backgroundcolor,
+                pointRadius: 0,
+                borderColor: borderColor
         }]
-        }
-        , options: {
+        },
+        options: {
             scales: {
                 yAxes: [{
-                    ticks: {
-                        beginAtZero: beginAtZero
-                    }
+                        ticks: {
+                            beginAtZero: beginAtZero
+                        }
                 }
                 ]
             }
