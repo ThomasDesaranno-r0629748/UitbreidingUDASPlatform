25c9498b2fb54a8dbb7b322b64dbc8f2b02daaf7
RemidelaKethulledeRyhove-r0624876 <remidelakethullederyhove@student.ucll.be>
2019-02-21 15:31:43 +0100

    extrafix

--- leafletmap.js
+++ leafletmap.js
@@ -442,35 +442,38 @@
              document.getElementById("map").style.height = "50%";
              document.getElementById("legendCollection").style.bottom = "52%";
              document.getElementById("chartHistory").style.visibility = "hidden";
-             
-             
-                 console.log("testchart");
-                 let tempChart2 = document.getElementById("tempChart2").getContext('2d');
-                 console.log(tempChart2 + "whuuutnfeosdfj");
-                 let pressureChart2 = document.getElementById("pressureChart2").getContext('2d');
-                 let O3Chart2 = document.getElementById("O3Chart2").getContext('2d');
-                 let PM1Chart2 = document.getElementById("PM1Chart2").getContext('2d');
+             document.getElementById("chartCollection").style.visibility = "hidden";
+             document.getElementById("history").style.visibility = "hidden";
 
-                 var chartLabels2 = [];
-                 var chartDataTemp2 = [];
-                 var chartDataPressure2 = [];
-                 var chartDataO32 = [];
-                 var chartDataPM12 = [];
 
-                 d3.json("MetingenSensor10.json", function (data) {
-                     data.forEach(function (d) {
-                         chartLabels.push(d.time);
-                         chartDataTemp.push(d.s1);
-                         chartDataPressure.push(d.s2);
-                         chartDataO3.push(d.s3);
-                         chartDataPM1.push(d.s4);
-                     })
+             console.log("testchart");
+             let tempChart2 = document.getElementById("tempChart2").getContext('2d');
+             console.log(tempChart2 + "whuuutnfeosdfj");
+             let pressureChart2 = document.getElementById("pressureChart2").getContext('2d');
+             let O3Chart2 = document.getElementById("O3Chart2").getContext('2d');
+             let PM1Chart2 = document.getElementById("PM1Chart2").getContext('2d');
+
+             var chartLabels2 = [];
+             var chartDataTemp2 = [];
+             var chartDataPressure2 = [];
+             var chartDataO32 = [];
+             var chartDataPM12 = [];
+
+             d3.json("http://localhost:8080/Controller?action=returnLast24hData", function (data) {
+                 console.log(data)
+                 data.forEach(function (d) {
+                     chartLabels.push(d.time);
+                     chartDataTemp.push(d.so2);
+                     chartDataPressure.push(d.no2);
+                     chartDataO3.push(d.o3);
+                     chartDataPM1.push(d.pm10);
                  })
+             })
 
-                 setTimeout(createChart, 500, chartLabels2, chartDataTemp2, tempChart2, 'SO2', 'rgba(255, 255, 0, 0.58)', true, '#989800');
-                 setTimeout(createChart, 500, chartLabels2, chartDataPressure2, pressureChart2, 'NO2', 'rgba(255, 0, 0, 0.58)', true, '#980000');
-                 setTimeout(createChart, 500, chartLabels2, chartDataPressure2, O3Chart2, 'O3', 'rgba(0, 255, 10, 0.58)', true, '#009806');
-                 setTimeout(createChart, 500, chartLabels2, chartDataPressure2, PM1Chart2, 'PM1', 'rgba(0, 245, 255, 0.58)', true, '#009298');
+             setTimeout(createChart, 500, chartLabels2, chartDataTemp2, tempChart2, 'SO2', 'rgba(255, 255, 0, 0.58)', true, '#989800');
+             setTimeout(createChart, 500, chartLabels2, chartDataPressure2, pressureChart2, 'NO2', 'rgba(255, 0, 0, 0.58)', true, '#980000');
+             setTimeout(createChart, 500, chartLabels2, chartDataPressure2, O3Chart2, 'O3', 'rgba(0, 255, 10, 0.58)', true, '#009806');
+             setTimeout(createChart, 500, chartLabels2, chartDataPressure2, PM1Chart2, 'PM1', 'rgba(0, 245, 255, 0.58)', true, '#009298');
          }
 
          function closeCompare() {
@@ -481,6 +484,7 @@
              document.getElementById("compare").style.visibility = "visible";
              document.getElementById("map").style.height = "100%";
              document.getElementById("legendCollection").style.bottom = "5%";
+             document.getElementById("comparePart").style.display = "none";
              comparegraphs = 0;
          }
 
